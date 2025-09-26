7e0eb8f02731a342ce754376d4bdb67783ed32d3
irha-web <irhazulfia348@gmail.com>
2025-09-26 09:50:41 +0700

    membuat project login

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+</head>
+<body>
+    <h2>Form Login</h2>
+
+    <Form method="post">
+        Username: <input type="text" name="username" required><br><br>
+        Username: <input type="text" name="password" required><br><br>
+        <button type="submit">Login</button>
+</from>
+</body>
+</html>
